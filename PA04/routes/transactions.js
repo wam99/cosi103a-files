2e85ec82ec3f7475d57579af38f60ef8f8801d46
wam99 <123836439+wam99@users.noreply.github.com>
2023-04-18 21:20:12 -0400

    transactioning it up

---
+++ PA04/routes/transactions.js
@@ -0,0 +1,59 @@
+/*
+  todo.js -- Router for the ToDoList
+*/
+const express = require('express');
+const router = express.Router();
+const TransactionItem = require('../models/TransactionItem')
+const User = require('../models/User')
+
+
+/*
+this is a very simple server which maintains a key/value
+store using an object where the keys and values are lists of strings
+
+*/
+
+isLoggedIn = (req,res,next) => {
+  if (res.locals.loggedIn) {
+    next()
+  } else {
+    res.redirect('/login')
+  }
+}
+
+// get the value associated to the key
+router.get('/transactions/',
+  isLoggedIn,
+  async (req, res, next) => {
+      const show = req.query.show
+      const completed = show=='completed'
+      let items=[]
+      if (show) { // show is completed or todo, so just show some items
+        items = 
+          await TransactionItem.find({userId:req.user._id, completed})
+                        .sort({completed:1,priority:1,createdAt:1})
+      }else {  // show is null, so show all of the items
+        items = 
+          await TransactionItem.find({userId:req.user._id})
+                        .sort({completed:1,priority:1,createdAt:1})
+
+      }
+            res.render('toDoList',{items,show,completed});
+});
+
+/* add the value in the body to the list associated to the key */
+router.post('/transactions',
+  isLoggedIn,
+  async (req, res, next) => {
+      const todo = new ToDoItem(
+        {item:req.body.item,
+         createdAt: new Date(),
+         completed: false,
+         priority: parseInt(req.body.priority),
+         userId: req.user._id
+        })
+      await todo.save();
+      res.redirect('/todo')
+});
+
+module.exports = router;
